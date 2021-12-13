49f67d8fcac2bd4d4fba33a439a84fce91074288
Abhijeet Kumar <81406799+abkumarofficial@users.noreply.github.com>
2021-12-14 01:11:09 +0530

    Add files via upload

---
+++ reactnd-contacts-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+
+class App extends Component {
+  state = {
+    contacts : [],
+    screen: 'create'
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+    .then((contacts) => {
+      console.log('data reccieved', contacts);
+      this.setState(() => ({
+        contacts
+      }))
+    })
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+    ContactsAPI.remove(contact)
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.state.screen === 'list' && <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}></ListContacts>}
+        {this.state.screen === 'create' && <CreateContact></CreateContact>}
+      </div>
+    );
+  }
+}
+
+export default App;
